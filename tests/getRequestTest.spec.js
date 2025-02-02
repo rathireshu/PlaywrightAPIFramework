09c1cb37da5f8e567e28b1e12b5b3bfedfc40e4d
rathireshu <rathi.reshu@gmail.com>
2025-02-02 09:47:08 -0500

    Update Post req

--- tests/getRequestTest.spec.js
+++ tests/getRequestTest.spec.js
@@ -5,12 +5,20 @@ const testData = require('../apiTestData.json');
 test.only('APIs Testing', async ({request}) => {
 
  const getResponse = await request.get(`${testData.baseUrl}`, { params: { name: '2' } });
-
+ // Validate status code
  expect(getResponse.status()).toBe(200)
- const data =  await getResponse.json();
- console.log(data);
- //schema validation
 
+// Validate response text message
+const statusMsg = getResponse.statusText();
+console.log('status message is :'+statusMsg);
+expect.soft(statusMsg).toBe('OK');
+expect(statusMsg).toContain('OK');
+
+const responseBody = await getResponse.text();
+//console.log('response body is :'+responseBody);
+
+ //Schema validation
+ const data =  await getResponse.json();
  expect(data.data[0]).toHaveProperty('id');
  expect(data.data[0]).toHaveProperty('email');
  expect(data.data[0]).toHaveProperty('first_name');
@@ -21,7 +29,8 @@ test.only('APIs Testing', async ({request}) => {
 const postResponse = await request.post('https://reqres.in/api/users',{data : {name: 'testreq' , job: 'apitest'}});
 expect(postResponse.status()).toBe(201);
 const postData =  await postResponse.json();
- console.log(postData);
+ //console.log(postData);
+ //expect(postData.title).toBe('New Post');
  expect(postData.name).toBe('testreq');
 
 
