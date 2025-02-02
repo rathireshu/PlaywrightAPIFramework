1dbb614c4e83cf46de6d19a0901b32f9247653c8
rathireshu <rathi.reshu@gmail.com>
2025-02-02 00:55:59 -0500

    added post api

--- tests/getRequestTest.spec.js
+++ tests/getRequestTest.spec.js
@@ -1,13 +1,28 @@
 const {test, expect} = require('@playwright/test');
 
+const testData = require('../apiTestData.json');
 
-test.only('getRequestTest', async ({request}) => {
+test.only('APIs Testing', async ({request}) => {
 
- const response = await request.get('https://reqres.in/api/users');
+ const getResponse = await request.get(`${testData.baseUrl}`, { params: { name: '2' } });
 
- expect(response.status()).toBe(200)
+ expect(getResponse.status()).toBe(200)
+ const data =  await getResponse.json();
+ console.log(data);
+ //schema validation
+
+ expect(data.data[0]).toHaveProperty('id');
+ expect(data.data[0]).toHaveProperty('email');
+ expect(data.data[0]).toHaveProperty('first_name');
+ expect(data.data[0]).toHaveProperty('last_name');
+ expect(data.data[0]).toHaveProperty('avatar');
 
 
+const postResponse = await request.post('https://reqres.in/api/users',{data : {name: 'testreq' , job: 'apitest'}});
+expect(postResponse.status()).toBe(201);
+const postData =  await postResponse.json();
+ console.log(postData);
+ expect(postData.name).toBe('testreq');
 
 
 
